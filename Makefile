227c4a85fbd2707ad858a2c7894b7b1035335e8c
Yoinksawse <mayuchen334@gmail.com>
2025-05-27 22:47:11 +0800

    Bootloader: Hello World!

--- Makefile
+++ Makefile
@@ -1,35 +1,37 @@
-HOME_DIR := $(HOME)/BeckerOS
+HOME_DIR := .
 OS_NAME := BeckerOS
 ISO_OUTPUT := $(OS_NAME).iso
 
-BOOT_SRC := $(HOME_DIR)/src/Boot.asm
-BOOT_BIN := $(HOME_DIR)/bin/Boot.bin
-LOADER_SRC := $(HOME_DIR)/src/Loader.asm
-LOADER_OBJ := $(HOME_DIR)/bin/Loader.o
+BOOT_SRC := $(HOME_DIR)/src/boot.asm
+BOOT_BIN := $(HOME_DIR)/bin/boot.bin
+LOADER_SRC := $(HOME_DIR)/src/loader.asm
+LOADER_OBJ := $(HOME_DIR)/bin/loader.o
 
 KERNEL_SRC := $(HOME_DIR)/src/kernel_main.c
 KERNEL_OBJ := $(HOME_DIR)/bin/kernel_main.o
 KERNEL_ELF := $(HOME_DIR)/bin/kernel_main.elf
 KERNEL_BIN := $(HOME_DIR)/bin/kernel_main.bin
 
-all: build-BootLoader build-kernel build-iso run clean
+all: bootloader kernel iso clean
+	qemu-system-i386 -cdrom $(ISO_OUTPUT) -boot d -m 512
 
-build-BootLoader:
+test: bootloader
+	qemu-system-i386 -fda bin/boot.bin -boot a -m 512
+
+bootloader:
 	nasm -f bin $(BOOT_SRC) -o $(BOOT_BIN)
 	nasm -f elf32 $(LOADER_SRC) -o $(LOADER_OBJ)
 
-build-kernel: build-BootLoader 
+kernel: bootloader 
 	i686-elf-gcc -ffreestanding -m32 -c $(KERNEL_SRC) -o $(KERNEL_OBJ) 
 	i686-elf-ld -Ttext 0xA000 -o $(KERNEL_ELF) $(LOADER_OBJ) $(KERNEL_OBJ)
 	i686-elf-objcopy -O binary $(KERNEL_ELF) $(KERNEL_BIN)
 
-build-iso: build-kernel
+iso: bootloader kernel
+	cp bin/boot.bin .  
 	mkisofs -o $(ISO_OUTPUT) -V "$(OS_NAME)" \
-		-b bin/Boot.bin -no-emul-boot -boot-load-size 4 -boot-info-table \
-		$(HOME_DIR)
-
-run: build-iso
-	qemu-system-x86_64 -cdrom $(ISO_OUTPUT)
-
+		-b boot.bin -no-emul-boot -boot-load-size 4 -boot-info-table \
+		bin
+	rm boot.bin
 clean:
 	rm -f $(BOOT_BIN) $(LOADER_OBJ) $(KERNEL_OBJ) $(KERNEL_ELF) $(KERNEL_BIN)
---
+++ src/boot.asm
@@ -0,0 +1,58 @@
+;stage 1 of booting
+;-real mode: 
+;-protected mode: run Loader (loads kernel elf)
+;================================16-bit real mode==============================================
+;clear registers, disable interrupts, (enable a20line), load gdt
+
+[bits 16] 
+[org 0x7C00]
+
+;BEGIN TELETYPE PRINT FUNCTION (Fooling around)
+print:                          ;print: parameter is string in si
+    push si                     ;record current data
+    push ax
+looper:
+    lodsb                       ;loads next byte in si to al, si++
+    or al, al                   ;return if null
+    jz .done
+    mov ah, 0x0E                ;teletype output
+    int 0x10                    ;bios interrupt print
+    jmp looper                   ;loop
+.done:
+    pop ax
+    pop si
+    ret
+;END TELETYPE PRINT FUNCTION
+
+start:
+    cli                             ;clear interrupt flags + disable
+    mov ax, 0x00                    ;clear used registers
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7c00 - 0x100          ;setup stack below bootloader 
+    sti                             ;enable interrupts
+    
+    mov si, msg
+    call print
+end:
+    cli                             ;clear interrupts
+    hlt                             ;halt cpu work
+;==============================entering protected mode=========================================
+
+;CPU mode is determined by bit 0 of cr0: 0 = real Mode, 1 = Protected Mode 
+mov eax, cr0 ;make cr0 become 1
+or eax, 1
+mov cr0, eax
+
+;=============================32-bit protected mode============================================
+;run Loader (loads kernel elf), 
+;[bits 32]
+
+
+
+;=============================data segment=====================================================
+msg db 'Booting...', 0              ;print message (null at end)
+
+times 510 - ($ - $$) db 0           ;memset remaining of 1st segment to 0
+dw 0xaa55                           ;last 2 bytes: MBR signature
